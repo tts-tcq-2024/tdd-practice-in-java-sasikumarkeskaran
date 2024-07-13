de29eec53277c1cf9631dcec78d6c4826626091c
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-07-13 18:45:26 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -1,27 +1,55 @@
 package TddPracticeInJava;
 
 public class StringCalculator {
+
     public int add(String input) {
-        if (input == null || input.isEmpty()) {
+        if (isEmpty(input)) {
             return 0;
         }
-        
-       
-        String delimiter = ",|\n";
-        if (input.startsWith("//")) {
-            int delimiterIndex = input.indexOf('\n');
-            delimiter = input.substring(2, delimiterIndex);
-            input = input.substring(delimiterIndex + 1);
+        String[] numbers = splitNumbers(input);
+        return calculateSum(numbers);
+    }
+
+    private boolean isEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private String[] splitNumbers(String input) {
+        if (hasCustomDelimiter(input)) {
+            return splitWithCustomDelimiter(input);
         }
-        
-        String[] numbers = input.split(delimiter);
+        return splitWithDefaultDelimiter(input);
+    }
+
+    private boolean hasCustomDelimiter(String input) {
+        return input.startsWith("//");
+    }
+
+    private String[] splitWithCustomDelimiter(String input) {
+        int delimiterIndex = input.indexOf('\n');
+        String delimiter = input.substring(2, delimiterIndex);
+        String numbersPart = input.substring(delimiterIndex + 1);
+        return numbersPart.split(delimiter);
+    }
+
+    private String[] splitWithDefaultDelimiter(String input) {
+        String delimiter = ",|\n";
+        return input.split(delimiter);
+    }
+
+    private int calculateSum(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
-            int num = Integer.parseInt(number);
-            if (num <= 1000) {
-                sum += num;
-            }
+            sum += toValidNumber(number);
         }
         return sum;
+    }
+
+    private int toValidNumber(String number) {
+        int num = Integer.parseInt(number);
+        if (num > 1000) {
+            return 0;
+        }
+        return num;
     }
 }
