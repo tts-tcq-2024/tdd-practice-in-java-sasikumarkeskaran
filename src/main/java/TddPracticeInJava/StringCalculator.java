1b3dfd63e35ddfccda5fb53672f54d35911fdbf8
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2024-07-13 18:41:30 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -5,7 +5,23 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        return 0;
+        
+       
+        String delimiter = ",|\n";
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf('\n');
+            delimiter = input.substring(2, delimiterIndex);
+            input = input.substring(delimiterIndex + 1);
+        }
+        
+        String[] numbers = input.split(delimiter);
+        int sum = 0;
+        for (String number : numbers) {
+            int num = Integer.parseInt(number);
+            if (num <= 1000) {
+                sum += num;
+            }
+        }
+        return sum;
     }
 }
-
